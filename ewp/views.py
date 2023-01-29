0e6137966cc2957710ee10e0fb4b95e5f4734827
Hamza <202100978@zewailcity.edu.eg>
2023-01-29 15:44:32 +0200

    created an app with name ewp (educational web page), setted time zone to GMT in settings.py, created a templates and static dirctories, and added 2 pages the /home and / about

---
+++ ewp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def home(request):
+    return HttpResponse('<h1>HOME PAGE</h1>')
