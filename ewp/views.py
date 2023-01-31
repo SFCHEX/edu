9ba84bc7bb4dc340c62dc948ce659eb147ea8b2a
Hamza <202100978@zewailcity.edu.eg>
2023-01-31 11:11:27 +0200

    Delete,Update and Create Class views for courses

--- ewp/views.py
+++ ewp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Course
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 
 def index(request):
@@ -18,8 +19,42 @@ def home(request):
 def login(request):
     return render(request, 'ewp/login.html')
 
-def archive(request):
-    context = {
-        'Courses': Course.objects.all(),
-    }
-    return render(request,'ewp/archive.html',context)
+
+class CourseListView(ListView):
+    model = Course
+    template_name = 'ewp/archive.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'Courses'
+    ordering = ['-date_posted']
+
+class CourseCreateView(LoginRequiredMixin,CreateView):
+    model = Course
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class CourseUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Course
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        course= self.get_object()
+        if self.request.user == course.author:
+            return True
+        return False
+
+
+class CourseDeletView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Course
+
+    def test_func(self):
+        course = self.get_object()
+        if self.request.user == course.author:
+            return True
+        return False
